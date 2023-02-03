e098f45b4f94ff0138693c675d78cb0e591edbf9
jyoung0930 <jaronyoung1@gmail.com>
2023-02-03 14:59:55 -0600

    Continued Development
    Added more logic and style to input when form is submitted.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,19 +4,32 @@ import Form from "./Form";
 
 export default function App() {
   const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState("dictionary");
+  const [error, setError] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [empty, setIsEmpty] = useState(false);
 
   useEffect(() => {
-    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/pen")
-      .then((res) => (res.ok ? res.json() : console.log("error")))
-      .then((data) => setData(data[0]));
-  }, []);
+    // fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
+    //   .then((res) => (res.ok ? res.json() : console.log("error")))
+    //   .then((data) =>
+    //     data === undefined ? setError((prev) => !prev) : setData(data[0])
+    //   );
+  }, [submitted]);
 
   console.log(data);
+  console.log(error);
 
   return (
     <div id="main_container">
       <Header />
-      <Form />
+      <Form
+        setInputValue={setInputValue}
+        value={inputValue}
+        setSubmitted={setSubmitted}
+        empty={empty}
+        setIsEmpty={setIsEmpty}
+      />
     </div>
   );
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,9 +1,29 @@
 import searchIcon from "../assets/images/icon-search.svg";
 
-export default function Form() {
+export default function Form({
+  setInputValue,
+  value,
+  setSubmitted,
+  empty,
+  setIsEmpty,
+}) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSubmitted((prev) => !prev);
+    value === "" ? setIsEmpty(true) : setIsEmpty(false);
+  }
+
+  console.log(empty);
   return (
-    <form>
-      <input type="text" />
+    <form onSubmit={handleSubmit}>
+      <input
+        className={empty ? "error" : ""}
+        type="text"
+        placeholder="Search for any word..."
+        onChange={(e) => setInputValue(e.target.value)}
+        value={value}
+        maxLength="30"
+      />
       <img src={searchIcon} alt="icon" />
     </form>
   );
